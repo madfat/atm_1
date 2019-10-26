4506f320b9c272142788fee6287b5db09ebb497b
madfat <madfat@gmail.com>
2019-10-26 07:20:26 +0700

    minor fix

--- src/cdc/atm/Atm.java
+++ src/cdc/atm/Atm.java
@@ -5,7 +5,6 @@ import src.cdc.atm.view.WelcomeScreen;
 
 public class Atm {
     public static void main(String[] args) {
-        Account loggedInAccount = new Account();
         WelcomeScreen welcomeScreen = new WelcomeScreen();
         do {
             welcomeScreen.show();
