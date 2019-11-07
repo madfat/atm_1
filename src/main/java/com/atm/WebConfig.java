38c22eb07f10daf3ce731aee5e3f0c24dc283c6d
madfat <madfat@gmail.com>
2019-11-07 10:47:26 +0700

    JSF configuration

---
+++ src/main/java/com/atm/WebConfig.java
@@ -0,0 +1,28 @@
+package com.atm;
+
+import org.ocpsoft.rewrite.servlet.DispatcherType;
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.faces.webapp.FacesServlet;
+import java.util.EnumSet;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean(servlet, "*.jsf");
+    }
+
+    @Bean
+    public FilterRegistrationBean rewriteFilter(){
+        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR));
+        rwFilter.addUrlPatterns("/*");
+        return rwFilter;
+    }
+}
